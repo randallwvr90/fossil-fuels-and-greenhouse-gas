47608f0ddc07fb46285a1a205bb6631421c25944
Randy Weaver <randallwvr90@gmail.com>
2022-01-05 13:58:49 -0500

    add basic html templates using bootstrap

--- app.py
+++ app.py
@@ -1,35 +1,41 @@
 # flask imports
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 ########################## Flask ##############################
 
 app = Flask(__name__)
 
 ##################### Route - Home ############################
+
+
 @app.route('/')
 def index():
+    heading: str = '''
+    Fossil Fuel Consumption and 
+    Greenhouse Gas Emission Dashboard
+    '''
+    heading2: str = 'Pages:'
+    info: str = '/api/v1.0/consumption'
+    info2: str = '/api/v1.0/emissions'
+    return render_template('index.html', heading=heading, heading2=heading2, info=info, info2=info2)
 
-    return (
-        f'<h1>Fossil Fuel Consumption and Greenhouse Gass Emission Dashboard</h1><br>'
-        f'<ul><li> /api/v1.0/consumption </li><br>'
-        f'<li> /api/v1.0/emissions </li></ul>'
-        )
 
 ############## Route - Fossil Fuel Consumption ##################
+
+
 @app.route('/api/v1.0/consumption')
 def consumption():
-
-    return(
-        f'<h1>Fossil Fuel Consumption by Country</h1>'
-        )
+    heading: str = 'Fossil Fuel Consumption by Country'
+    info: str = 'This is the consumption page.'
+    return render_template("page1.html", heading=heading, info=info)
 
 ############## Route - Greenhouse Gas Emissions ##################
     @app.route('/api/v1.0/emissions')
     def emissions():
 
         return(
-                f'<h1>Greenhouse Gas Emissions by Country</h1>'
-              )
+            f'<h1>Greenhouse Gas Emissions by Country</h1>'
+        )
 
 
 if __name__ == "__main__":
