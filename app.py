6953e2b8118f879527908d03f5d0275b6b14cd30
Randy Weaver <randallwvr90@gmail.com>
2022-01-14 15:03:58 -0500

    Add Danelle's app.py functions

--- app.py
+++ app.py
@@ -238,6 +238,155 @@ def get_global_emissions():
     close_connection(conn)
     return jsonify(results)
 
+# -------------------------------------------------------------------- #
+#                           Route - allYears
+# -------------------------------------------------------------------- #
+
+
+@app.route('/api/v1.0/allYears')
+def allYears():
+
+    heading: str = '''
+    Fossil Fuel Consumption and 
+    Greenhouse Gas Emission Dashboard
+    '''
+    heading2: str = 'Pages:'
+    return render_template(
+        'allYears3.html',
+        heading=heading, heading2=heading2
+    )
+
+
+# -------------------------------------------------------------------- #
+#                           Route - "countries2"
+# -------------------------------------------------------------------- #
+
+
+@app.route("/api/v1.0/countries2")
+def get_countries2():
+    """Return Countries data as json"""
+    conn = open_connection()
+    cursor = conn.cursor()
+    cursor.execute("""Select country from country_master order by 1""")
+    results = cursor.fetchall()
+    cursor.close()
+
+    close_connection(conn)
+    return jsonify(results)
+
+
+# -------------------------------------------------------------------- #
+#                 Route - "allYears/emissions/<country>"
+# -------------------------------------------------------------------- #
+
+
+@app.route("/api/v1.0/allYears/emissions/<country>")
+def get_allEmissions(country):
+
+    conn = open_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        """select year, emission_value from co2_emission where country = (%s) """, (country,))
+    results = cursor.fetchall()
+    cursor.close()
+    close_connection(conn)
+
+    years = []
+    co2 = []
+    returnValue = []
+    for result in results:
+        years.append(result[0])
+        co2.append(result[1])
+
+    returnValue.append(years)
+    returnValue.append(co2)
+
+    return jsonify(returnValue)
+
+
+# -------------------------------------------------------------------- #
+#                 Route - "allYears/consumption/<country>"
+# -------------------------------------------------------------------- #
+
+
+@app.route("/api/v1.0/allYears/consumption/<country>")
+def get_Consumption(country):
+
+    conn = open_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        """select year, consumption_value from fuel_consumption where country = (%s) and fuel_type = 'oilcons_ej' """, (country,))
+    results1 = cursor.fetchall()
+    cursor.execute(
+        """select year, consumption_value from fuel_consumption where country = (%s) and fuel_type = 'gascons_ej' """, (country,))
+    results2 = cursor.fetchall()
+    cursor.execute(
+        """select year, consumption_value from fuel_consumption where country = (%s) and fuel_type = 'coalcons_ej' """, (country,))
+    results3 = cursor.fetchall()
+    cursor.execute(
+        """select year, consumption_value from fuel_consumption where country = (%s) and fuel_type = 'ethanol_cons_ej' """, (country,))
+    results4 = cursor.fetchall()
+    cursor.execute(
+        """select year, consumption_value from fuel_consumption where country = (%s) and fuel_type = 'biofuels_cons_ej' """, (country,))
+    results5 = cursor.fetchall()
+    cursor.execute(
+        """select year, consumption_value from fuel_consumption where country = (%s) and fuel_type = 'biodiesel_cons_ej' """, (country,))
+    results6 = cursor.fetchall()
+    cursor.close()
+    close_connection(conn)
+
+    years1 = []
+    fuel1 = []
+    for result in results1:
+        years1.append(result[0])
+        fuel1.append(result[1])
+
+    years2 = []
+    fuel2 = []
+    for result in results2:
+        years2.append(result[0])
+        fuel2.append(result[1])
+
+    years3 = []
+    fuel3 = []
+    for result in results3:
+        years3.append(result[0])
+        fuel3.append(result[1])
+
+    years4 = []
+    fuel4 = []
+    for result in results4:
+        years4.append(result[0])
+        fuel4.append(result[1])
+
+    years5 = []
+    fuel5 = []
+    for result in results5:
+        years5.append(result[0])
+        fuel5.append(result[1])
+
+    years6 = []
+    fuel6 = []
+    for result in results6:
+        years6.append(result[0])
+        fuel6.append(result[1])
+
+    returnValue = []
+    returnValue.append(years1)
+    returnValue.append(fuel1)
+    returnValue.append(years2)
+    returnValue.append(fuel2)
+    returnValue.append(years3)
+    returnValue.append(fuel3)
+    returnValue.append(years4)
+    returnValue.append(fuel4)
+    returnValue.append(years5)
+    returnValue.append(fuel5)
+    returnValue.append(years6)
+    returnValue.append(fuel6)
+
+    return jsonify(returnValue)
+
 
 def main():
     #----------Create DB------#
