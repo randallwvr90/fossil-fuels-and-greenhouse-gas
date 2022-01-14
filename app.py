6a0a3f4474a23d389456b537886fc7e5da23a100
jennylynnlong <88349512+jennylynnlong@users.noreply.github.com>
2022-01-14 13:38:04 -0500

    updating branch with main files

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 # flask imports
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template, jsonify
+import json
 import psycopg2
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
 
-from postgres_key import DB_USER,DB_KEY,DB_NAME
+from postgres_key import DB_USER, DB_KEY, DB_NAME
+from createDatabase import create_db
+
+
+# -------------------------------------------------------------------- #
+#                             DB functions
+# -------------------------------------------------------------------- #
+
+
+def open_connection():
+    conn = psycopg2.connect(host="localhost", port=5432,
+                            database=DB_NAME, user=DB_USER, password=DB_KEY)
+    return conn
+
+
+def close_connection(conn):
+    conn.close()
+
 
 # -------------------------------------------------------------------- #
 #                               Flask
 # -------------------------------------------------------------------- #
 app = Flask(__name__)
+
 
 # -------------------------------------------------------------------- #
 #                           Route - Home
@@ -16,64 +38,100 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #Connect to the database to get the list of countries
+    # Connect to the database to get the list of countries
 
     heading: str = '''
     Fossil Fuel Consumption and 
     Greenhouse Gas Emission Dashboard
     '''
-    heading2: str = 'Pages:'
-    content_1_title: str = 'Fossil Fuel Consumption'
-    content_1_location: str = '/api/v1.0/consumption'
-    content_2_title: str = 'CO2 Emissions'
-    content_2_location: str = '/api/v1.0/emissions'
+    #heading2: str = 'Pages:'
+    #content_1_title: str = 'Fossil Fuel Consumption'
+    #content_1_location: str = '/api/v1.0/consumption'
+    #content_2_title: str = 'CO2 Emissions'
+    #content_2_location: str = '/api/v1.0/emissions'
     return render_template(
         'index.html',
-        heading=heading, heading2=heading2,
-        content_1_title=content_1_title, content_1_location=content_1_location,
-        content_2_title=content_2_title, content_2_location=content_2_location
+        heading=heading
+        #content_1_title=content_1_title, content_1_location=content_1_location,
+        #content_2_title=content_2_title, content_2_location=content_2_location
     )
+
+
+# -------------------------------------------------------------------- #
+#                Route - get the list of countries
+# -------------------------------------------------------------------- #
+
 
 @app.route("/api/v1.0/countries")
 def get_countries():
-    """Return the justice league data as json"""
+    """Return Countries data as json"""
     conn = open_connection()
-
-    print("Hello")
-
-    cursor  = conn.cursor()
-    cursor.execute("""Select country from country_master""")
+    cursor = conn.cursor()
+    cursor.execute("""Select country from country_master order by 1""")
     results = cursor.fetchall()
-    print(results)
     cursor.close()
 
     close_connection(conn)
     return jsonify(results)
+
+
+# -------------------------------------------------------------------- #
+#                 Route - get the list of year ranges
+# -------------------------------------------------------------------- #
+
 
 @app.route("/api/v1.0/groupings")
 def get_year_groupings():
-    """Return the justice league data as json"""
+    """Return Year Ranges data as json"""
     conn = open_connection()
-
-    print("Hello")
-
-    cursor  = conn.cursor()
-    cursor.execute("""select distinct year_range from fuel_consumption""")
+    cursor = conn.cursor()
+    cursor.execute(
+        """select distinct year_range from fuel_consumption order by year_range asc""")
     results = cursor.fetchall()
-    print(results)
     cursor.close()
 
     close_connection(conn)
     return jsonify(results)
 
 
-def open_connection():
-    conn = psycopg2.connect(host="localhost", port = 5432, database=DB_NAME, user=DB_USER, password=DB_KEY)
-    return conn
+# -------------------------------------------------------------------- #
+#           Route - gdp for a country in a specified year range
+# -------------------------------------------------------------------- #
 
-def close_connection(conn):
-    conn.close()
-    
+
+@app.route("/api/v1.0/gdp/<country>/<year_range>")
+def get_gdp(country, year_range):
+    """Return GDP data as json"""
+    conn = open_connection()
+
+    years = year_range.split('-')
+
+    fromYear = int(years[0])
+    toYear = int(years[1])
+
+    cursor = conn.cursor()
+    cursor.execute("""select year, gdp from gdp where country = (%s) and year between (%s) and (%s) """,
+                   (country, fromYear, toYear))
+    results = cursor.fetchall()
+    # print(results)
+    cursor.close()
+    close_connection(conn)
+
+    years = []
+    gdpValue = []
+    returnValue = []
+
+    # Loop through the result to seperate the year and the gdp value
+    for result in results:
+        years.append(result[0])
+        gdpValue.append(result[1])
+
+    returnValue.append(years)
+    returnValue.append(gdpValue)
+
+    # print(returnValue)
+    return jsonify(returnValue)
+
 
 # -------------------------------------------------------------------- #
 #                  Route - Fossil Fuel Consumption
@@ -81,7 +139,7 @@ def close_connection(conn):
 
 
 @app.route('/api/v1.0/consumption')
-def consumption():
+def consumption(country, year_range):
     title: str = 'Fossil Fuel Consumption by Country'
     heading: str = 'Fossil Fuel Consumption by Country'
     info: str = 'This is the consumption page.'
@@ -98,22 +156,96 @@ def consumption():
         content_2_title=content_2_title, content_2_location=content_2_location
     )
 
+
 # -------------------------------------------------------------------- #
 #                  Route - Greenhouse Gas Emissions
 # -------------------------------------------------------------------- #
 
 
-@app.route('/api/v1.0/emissions')
-def emissions():
+@app.route('/api/v1.0/emissions/<country>/<year_range>')
+def emissions(country, year_range):
 
-    return(
-        f'<h1>Greenhouse Gas Emissions by Country</h1>'
+    conn = open_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        """select year,emission_value from co2_emission where country = (%s) and year_range=(%s)""",
+        (country, year_range)
+    )
+    results = cursor.fetchall()
+    # print(results)
+    cursor.close()
+    close_connection(conn)
+
+    years = []
+    EmissionValue = []
+    returnValue = []
+
+    # Loop through the result to seperate the year and the gdp value
+    for result in results:
+        years.append(result[0])
+        EmissionValue.append(result[1])
+
+    returnValue.append(years)
+    returnValue.append(EmissionValue)
+
+    # print(returnValue)
+    return jsonify(returnValue)
+
+
+# -------------------------------------------------------------------- #
+#                      Route - emissions map
+# -------------------------------------------------------------------- #
+
+
+@app.route("/api/v1.0/emissions_map")
+def emissions_map():
+    return render_template(
+        'map_page.html'
     )
 
 
-def main():
-    '''Run the Flask app.'''
+# -------------------------------------------------------------------- #
+#                      Route - get geojson
+# -------------------------------------------------------------------- #
 
+
+@app.route("/api/low_res_world")
+def low_res_world():
+    geo_json_path = './static/data/low_res_world.geo.json'
+    f = open(geo_json_path, "r")
+    # geo_json = f.readline()
+    geo_json_dict = json.load(f)
+    f.close()
+    print(type(geo_json_dict))
+    return jsonify(geo_json_dict)
+
+
+# -------------------------------------------------------------------- #
+#                      Route - get global emissions
+# -------------------------------------------------------------------- #
+
+
+@app.route("/api/v1.0/get_global_emissions")
+def get_global_emissions():
+    """Get emissions data as json"""
+    conn = open_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        """select  emission_value, country_id from co2_emission where year = 2020"""
+    )
+    results = cursor.fetchall()
+    cursor.close()
+
+    close_connection(conn)
+    return jsonify(results)
+
+
+def main():
+    #----------Create DB------#
+    print("Creating Database")
+    create_db(DB_USER, DB_KEY, DB_NAME)
+    print("Database Created")
+    '''Run the Flask app.'''
     app.run(debug=True)
 
 
